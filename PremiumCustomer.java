320654b8e46517f2bfe70d507adcfe0f5ac6675d
LaDarius Patrick <165985719+LDPatrick30@users.noreply.github.com>
2024-04-13 14:38:18 -0500

    Update PremiumCustomer.java

--- PremiumCustomer.java
+++ PremiumCustomer.java
@@ -1,14 +1,15 @@
 public class PremiumCustomer extends Customer{
-    public PremiumCustomer(String firstName,String lastName,int phoneNumber,String email,String address){
-        super(firstName, lastName, email, address, phoneNumber);
+    public PremiumCustomer(String name,int phoneNumber,String address) {
+        super(name, address, phoneNumber);
     }
-
     public void premiumPayCoffee(){
-        System.out.println(getFirstName()+ " "+getLastName()+ " "+"Premium customer can pay for the coffee using bitcoin, credit card, or cash.");
-        System.out.println("A 10 percent discount will be applied to the price");
+        System.out.println("Premium customer get a 10 percent discount and can pay for the coffee using bitcoin, credit card, or cash.");
+
+
     }
     @Override
     public void payCoffee(){
         premiumPayCoffee();
+
     }
 }
