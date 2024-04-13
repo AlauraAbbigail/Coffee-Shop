e6b2b0afde8cf5728704168f58a4e74dee8ce1bf
LaDarius Patrick <165985719+LDPatrick30@users.noreply.github.com>
2024-04-13 14:40:31 -0500

    Update CustomerGui.java

--- CustomerGui.java
+++ CustomerGui.java
@@ -3,9 +3,10 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-
 public class CustomerGui implements ActionListener {
+    RegularCustomer regular;
 
+    PremiumCustomer premiumCustomer;
     JFrame frame = new JFrame();
     //background image made by Alaura Buzbee
     ImageIcon pic = new ImageIcon(getClass().getResource("Coffee_Background.png"));
@@ -13,29 +14,22 @@ public class CustomerGui implements ActionListener {
     Image scaledImage = icon.getScaledInstance(700, 400, Image.SCALE_SMOOTH);
     JLabel background = new JLabel(new ImageIcon(scaledImage));
     JLabel title = new JLabel("Customer Info");
-    JLabel fName = new JLabel("First Name:");
-    JLabel lName = new JLabel("Last Name:");
-    JLabel resident= new JLabel("Address:");
+    JLabel fName = new JLabel("Name:");
+    JLabel address = new JLabel("Address:");
     JLabel phone = new JLabel("Phone:");
 
-    JLabel Email = new JLabel("Email:");
-    JLabel register = new JLabel("Register");
-    JLabel cancel = new JLabel("Cancel");
+    //RadioButton
     JRadioButton premium = new JRadioButton("Are you a Premium customer?");
-    JRadioButton regular = new JRadioButton();
-
 
     //TEXTFIELD
     JTextField fName1 = new JTextField();
-    JTextField lName1 = new JTextField();
-    JTextField resident1 = new JTextField();
+    JTextField resident = new JTextField();
     JTextField phone1 = new JTextField();
 
-    JTextField Email1 = new JTextField();
-
     //JBUTTON
-    JButton regButton = new JButton("Pay order");
+    JButton viewOrder = new JButton("View order");
     JButton cancelButton = new JButton("Cancel");
+
     CustomerGui(){
         title.setBounds(100,0,500,50);
         title.setFont(new Font(null,Font.BOLD,25));
@@ -51,26 +45,18 @@ public class CustomerGui implements ActionListener {
         fName.setFont(new Font(null,Font.BOLD,16));
         fName1.setBounds(150,90,300,20);
 
-        lName.setBounds(50,120,100,20);
-        lName.setFont(new Font(null,Font.BOLD,16));
-        lName1.setBounds(150,120,300,20);
+        address.setBounds(50,120,100,20);
+        address.setFont(new Font(null,Font.BOLD,16));
+        resident.setBounds(150,120,300,20);
 
-        resident.setBounds(50,150,100,20);
-        resident.setFont(new Font(null,Font.BOLD,16));
-        resident1.setBounds(150,150,300,20);
-
-        phone.setBounds(50,180,100,20);
+        phone.setBounds(50,150,100,20);
         phone.setFont(new Font(null,Font.BOLD,16));
-        phone1.setBounds(150,180,300,20);
-
-        Email.setBounds(50,210,100,20);
-        Email.setFont(new Font(null,Font.BOLD,16));
-        Email1.setBounds(150,210,300,20);
+        phone1.setBounds(150,150,300,20);
 
         // REG BUTTON
-        regButton.setBounds(50,320,100,40);
-        regButton.setFocusable(false);
-        regButton.addActionListener(this);
+        viewOrder.setBounds(50,320,100,40);
+        viewOrder.setFocusable(false);
+        viewOrder.addActionListener(this);
 
         // CANCEL BUTTON
         cancelButton.setBounds(300,320,100,40);
@@ -81,22 +67,19 @@ public class CustomerGui implements ActionListener {
         // to add label to frame
         frame.add(title);
         frame.add(fName);
-        frame.add(lName);
+        frame.add(address);
         frame.add(resident);
         frame.add(phone);
-        frame.add(Email);
-        frame.add(register);
-        frame.add(cancel);
+        frame.add(viewOrder);
         frame.add(premium);
         frame.add(background);
         //JTEXTFIELD
         frame.add(fName1);
-        frame.add(lName1);
-        frame.add(resident1);
+        frame.add(resident);
         frame.add(phone1);
-        frame.add(Email1);
+
         // JBUTTON
-        frame.add(regButton);
+
         frame.add(cancelButton);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(700,400);
@@ -106,7 +89,17 @@ public class CustomerGui implements ActionListener {
     }
     @Override
     public void actionPerformed(ActionEvent e) {
+        String name = fName1.getText();
+        String phone = phone1.getText();
+        String address = resident.getText();
         if(e.getSource()==premium){
+            premiumCustomer.payCoffee();
+        }
+
+    }
+}
+
+
 
 
         }
